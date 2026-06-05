da5510a4f79681985a40459a0ab222ea05fb4ddf
VISHNU PRIYA G <168539669+VishnuPriyaG13@users.noreply.github.com>
2026-06-05 17:48:52 +0530

    Add files via upload

---
+++ June_05/working_numpy.py
@@ -0,0 +1,29 @@
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print(arr)
+
+print(arr + 5)
+print(arr * 2)
+print(np.sum(arr))
+print(np.max(arr))
+print(np.min(arr))
+print(np.mean(arr))
+print(np.shape)
+
+
+#2D array
+arr1=np.array([
+    [10,20,30],
+    [40,50,60]
+])
+print(arr1)
+
+arr2=np.zeros((3,4))
+print(arr2)
+
+arr3=np.ones((2,3))
+print(arr3)
+
+arr=np.arange(1,11)
+print(arr)
+
